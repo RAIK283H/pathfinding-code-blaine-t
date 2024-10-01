40655a6a64dbc6591d1907f1085252c27652fd65
blaine-t <blaine-t@bathost.net>
2024-09-30 21:49:00 -0500

    Add precondition and postcondition

--- pathing.py
+++ pathing.py
@@ -16,13 +16,22 @@ def get_test_path():
 
 
 def get_random_path():
-    path = []
     graph_index = global_game_data.current_graph_index
     graph = graph_data.graph_data[graph_index]
 
-    current_node = 0
+    start_node = 0
     target_node = global_game_data.target_node[graph_index]
     exit_node = len(graph) - 1
+
+    # Precondition
+    # Ensure that there is a start node, a target node, and an exit node to target
+    assert graph[start_node]
+    assert graph[target_node]
+    assert graph[exit_node]
+
+    path = []
+    current_node = start_node
+
     while current_node != target_node:
         neighbors = graph[current_node][1]
         neighbors_count = len(neighbors)
@@ -34,14 +43,21 @@ def get_random_path():
         path.append(current_node)
 
         # If we reach the target node, we need to change the target to the exit node
-        # If we are at the exit node, we are done
-        # This ensures we don't skip the target by going directly to the exit node
         if current_node == target_node:
-            if current_node != exit_node:
-                target_node = exit_node
-            else:
-                return path
+            target_node = exit_node
 
+    # Postcondition
+    # Ensure in path that the first node is the start node
+    # the target node is in the path
+    # the last node is the exit node
+    # each node in the path is connected to the next node
+    assert path[0] == start_node
+    assert target_node in path
+    assert path[-1] == exit_node
+    for i in range(len(path) - 1):
+        assert path[i + 1] in graph[path[i]][1]
+
+    return path
 
 def get_dfs_path():
     return [1,2]
