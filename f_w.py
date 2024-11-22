d0ce0c934bd80b174bb960f6f752553736e9e817
blaine-t <blaine-t@bathost.net>
2024-11-21 21:02:52 -0600

    Fixed f_w path?

--- f_w.py
+++ f_w.py
@@ -6,7 +6,9 @@ import math
 def adjacency_list_to_matrix(index):
     adjacency_list = graph_data.graph_data[index]
     adjacency_list_size = len(adjacency_list)
-    matrix = [[None] * adjacency_list_size for i in range(adjacency_list_size)]
+    matrix = [[float('inf')] * adjacency_list_size for i in range(adjacency_list_size)]
+    for i in range(adjacency_list_size):
+        matrix[i][i] = 0
     for i, entry in enumerate(adjacency_list):
         neighbor = adjacency_list[i]
         neighbor_x = neighbor[0][0]
@@ -24,36 +26,31 @@ def floyd_warshall(W):
     for k in range(matrix_length):
         for i in range(matrix_length):
             for j in range(matrix_length):
-                # Otherwise error
-                if(W[i][k] and W[k][j]) and (W[i][j] is None or W[i][k] + W[k][j] < W[i][j]):
+                if W[i][k] < float('inf') and W[k][j] < float('inf') and W[i][k] + W[k][j] < W[i][j]:
                     W[i][j] = W[i][k] + W[k][j]
                     P[i][j] = k
     return P
 
 def floyd_warshall_path(P, i, j):
-    path = []
-    z = P[i][j]
-    
-    while z is not None:
-        path.insert(0, z)
-        z = P[i][z]
-    path.insert(0, i)
-    path.append(j)
-    
+    # Why does recursion work better than iteration AHHHH
+    if P[i][j] is None:
+        return [i, j]
+    path = floyd_warshall_path(P, i, P[i][j])
+    path.pop()
+    path.extend(floyd_warshall_path(P, P[i][j], j))
     return path
 
 def get_floyd_warshall_path(index):
     matrix = adjacency_list_to_matrix(index)
     start = 0
-    target = 1
+    target = 3
     end = len(matrix) - 1
     P = floyd_warshall(matrix)
     startToTarget = floyd_warshall_path(P, start, target)
-    print(startToTarget)
     targetToEnd = floyd_warshall_path(P, target, end)
-    print(targetToEnd)
+    startToTarget.pop(0)
     startToTarget.pop()
     startToTarget.extend(targetToEnd)
     return startToTarget
 
-print(get_floyd_warshall_path(0))
+print(get_floyd_warshall_path(4))
